b25d2eabc5f5594e954e845d283aa149a4021c0e
Anny Karoliny <anne.kar990@gmail.com>
2022-04-29 00:44:43 -0300

    funcionalidades

--- scripts.js
+++ scripts.js
@@ -1,10 +1,543 @@
-function verificar(){
-    var elemento = document.getElementsByClassName("check")
-    var nascer = document.getElementById("nascer")
+function check1(){
+    
+    
+    var list1 = document.getElementById('list1')
+    var check1 = document.getElementById('check1')
 
-    if(nascer.checked == true){
-        nascer.style.backgroundColor = "red"
+    if(check1.checked){
+        list1.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list1.style.backgroundColor = "#fff"
+
     }
+
+}
+function check2(){
+    
+    
+    var list2 = document.getElementById('list2')
+    var check2 = document.getElementById('check2')
+
+    if(check2.checked){
+        list2.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list2.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check3(){
+    
+    
+    var list3 = document.getElementById('list3')
+    var check3 = document.getElementById('check3')
+
+    if(check3.checked){
+        list3.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list3.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check4(){
+    
+    
+    var list4 = document.getElementById('list4')
+    var check4 = document.getElementById('check4')
+
+    if(check4.checked){
+        list4.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list4.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check5(){
+    
+    
+    var list5 = document.getElementById('list5')
+    var check5 = document.getElementById('check5')
+
+    if(check5.checked){
+        list5.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list5.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check6(){
+    
+    
+    var list6 = document.getElementById('list6')
+    var check6 = document.getElementById('check6')
+
+    if(check6.checked){
+        list6.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list6.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check7(){
+    
+    
+    var list7 = document.getElementById('list7')
+    var check7 = document.getElementById('check7')
+
+    if(check7.checked){
+        list7.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list7.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check8(){
+    
+    
+    var list8 = document.getElementById('list8')
+    var check8 = document.getElementById('check8')
+
+    if(check8.checked){
+        list8.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list8.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check9(){
+    
+    
+    var list9 = document.getElementById('list9')
+    var check9 = document.getElementById('check9')
+
+    if(check9.checked){
+        list9.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list9.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check10(){
+    
+    
+    var list10 = document.getElementById('list10')
+    var check10 = document.getElementById('check10')
+
+    if(check10.checked){
+        list10.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list10.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check11(){
+    
+    
+    var list11 = document.getElementById('list11')
+    var check11 = document.getElementById('check11')
+
+    if(check11.checked){
+        list11.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list11.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check12(){
+    
+    
+    var list12 = document.getElementById('list12')
+    var check12 = document.getElementById('check12')
+
+    if(check12.checked){
+        list12.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list12.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check13(){
+    
+    
+    var list13 = document.getElementById('list13')
+    var check13 = document.getElementById('check13')
+
+    if(check13.checked){
+        list13.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list13.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check14(){
+    
+    
+    var list14 = document.getElementById('list14')
+    var check14 = document.getElementById('check14')
+
+    if(check14.checked){
+        list14.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list14.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check15(){
+    
+    
+    var list15 = document.getElementById('list15')
+    var check15 = document.getElementById('check15')
+
+    if(check15.checked){
+        list15.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list15.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check16(){
+    
+    
+    var list16 = document.getElementById('list16')
+    var check16 = document.getElementById('check16')
+
+    if(check16.checked){
+        list16.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list16.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check17(){
+    
+    
+    var list17 = document.getElementById('list17')
+    var check17 = document.getElementById('check17')
+
+    if(check17.checked){
+        list17.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list17.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check18(){
+    
+    
+    var list18 = document.getElementById('list18')
+    var check18 = document.getElementById('check18')
+
+    if(check18.checked){
+        list18.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list18.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check19(){
+    
+    
+    var list19 = document.getElementById('list19')
+    var check19 = document.getElementById('check19')
+
+    if(check19.checked){
+        list19.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list19.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check20(){
+    
+    
+    var list20 = document.getElementById('list20')
+    var check20 = document.getElementById('check20')
+
+    if(check20.checked){
+        list20.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list20.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check21(){
+    
+    
+    var list21 = document.getElementById('list21')
+    var check21 = document.getElementById('check21')
+
+    if(check21.checked){
+        list21.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list21.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check22(){
+    
+    
+    var list22 = document.getElementById('list22')
+    var check22 = document.getElementById('check22')
+
+    if(check22.checked){
+        list22.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list22.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check23(){
+    
+    
+    var list23 = document.getElementById('list23')
+    var check23 = document.getElementById('check23')
+
+    if(check23.checked){
+        list23.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list23.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check24(){
+    
+    
+    var list24 = document.getElementById('list24')
+    var check24 = document.getElementById('check24')
+
+    if(check24.checked){
+        list24.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list24.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check25(){
+    
+    
+    var list25= document.getElementById('list25')
+    var check25 = document.getElementById('check25')
+
+    if(check25.checked){
+        list25.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list25.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check26(){
+    
+    
+    var list26 = document.getElementById('list26')
+    var check26 = document.getElementById('check26')
+
+    if(check26.checked){
+        list26.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list26.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check27(){
+    
+    
+    var list27 = document.getElementById('list27')
+    var check27 = document.getElementById('check27')
+
+    if(check27.checked){
+        list27.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list27.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check28(){
+    
+    
+    var list28 = document.getElementById('list28')
+    var check28 = document.getElementById('check28')
+
+    if(check28.checked){
+        list28.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list28.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check29(){
+    
+    
+    var list29 = document.getElementById('list29')
+    var check29 = document.getElementById('check29')
+
+    if(check29.checked){
+        list29.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list29.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check30(){
+    
+    
+    var list30 = document.getElementById('list30')
+    var check30 = document.getElementById('check30')
+
+    if(check30.checked){
+        list30.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list12.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check31(){
+    
+    
+    var list31 = document.getElementById('list31')
+    var check31 = document.getElementById('check31')
+
+    if(check31.checked){
+        list31.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list31.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check32(){
+    
+    
+    var list32 = document.getElementById('list32')
+    var check32 = document.getElementById('check32')
+
+    if(check32.checked){
+        list32.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list32.style.backgroundColor = "#fff"
+
+    }
+
+}
+function check33(){
+    
+    
+    var list33 = document.getElementById('list33')
+    var check33 = document.getElementById('check33')
+
+    if(check33.checked){
+        list33.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list33.style.backgroundColor = "#fff"
+
+    }
+
 }
 
-verificar()
+function check34(){
+    
+    
+    var list34 = document.getElementById('list34')
+    var check34 = document.getElementById('check34')
+
+    if(check34.checked){
+        list34.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list34.style.backgroundColor = "#fff"
+
+    }
+
+}
+
+function check35(){
+    
+    
+    var list35 = document.getElementById('list35')
+    var check35 = document.getElementById('check35')
+
+    if(check35.checked){
+        list35.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list35.style.backgroundColor = "#fff"
+
+    }
+
+}
+
+function check36(){
+    
+    
+    var list36 = document.getElementById('list36')
+    var check36 = document.getElementById('check36')
+
+    if(check36.checked){
+        list36.style.backgroundColor = "rgb(18, 226, 56)"
+
+    }else{
+        list36.style.backgroundColor = "#fff"
+
+    }
+
+}
